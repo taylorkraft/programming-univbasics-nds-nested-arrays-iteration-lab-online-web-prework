023fdfc360379d818d75c4c2f2723077403c5395
Taylor Kraft <taylorkraft16@gmail.com>
2019-10-30 01:44:11 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Nested Data Structures: Printing and Coalescing Data
+I'm # Nested Data Structures: Printing and Coalescing Data
 
 ## Learning Goals
 
--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,37 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+
+ empty_array = []
+ counter = 0 
+while counter < src.length do 
+  empty_array << "I love #{src[counter][0]} and #{src[counter][1]} on my pizza"
+  counter += 1 
+end 
+
+  empty_array
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  
+  empty_array = []
+  counter = 0
+while counter < src.length do
+  empty_array << src[counter].max
+  counter += 1 
+end
+
+  empty_array
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  
+  idx = 0 
+  total = 0
+while idx < src.length do 
+  if src[idx][0].even? && src[idx][1].even?
+total += src[idx].sum
 end
+  idx += 1
+end
+  total 
+end
+
